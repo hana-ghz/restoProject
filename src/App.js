2fc23362161e14f48ad40239dfbab57a9a049122
hanah <hana-ghz>
2020-08-21 03:05:06 +0100

    tags replaced with classes

--- src/App.js
+++ src/App.js
@@ -8,44 +8,26 @@ import Nav2 from './component/Nav2';
 import BuildBurger from './component/BuildBurger';
 import ContestForm from './component/ContestForm';
 import SliderComp from './component/SliderComp';
-<<<<<<< HEAD
 import Grid_meals from './component/Grid_meals';
 import Drinks from './component/Drinks';
+import Footer2  from './component/Footer2'
 
 
 function App() {
   return (
     <div className="App" style={{backgroundColor:'#fdebd0'}}>
-    <Nav2/>
-      <Router>
-                <Route path="/" exact component={SliderComp}></Route>
-                <Route path="/ContestForm" exact component={ContestForm}/>
-                <Route path="/BuildBurger" exact component={BuildBurger}/>
-      </Router>
-     <Grid_meals/>
-     <Drinks/>
-
-
-  <div style={{height:'600px'}}/>
-   
-  </div>
-
-=======
-import Footer2  from './component/Footer2'
-
-function App() {
-  return (
-    <div className="App">
+  
     	 <Nav2/> 
         <Router>
                   <Route path="/" exact component={SliderComp}></Route>
                   <Route path="/ContestForm" exact component={ContestForm}/>
                   <Route path="/BuildBurger" exact component={BuildBurger}/>
         </Router>
-        <div style={{height:"2000px"}}></div>
+        <Grid_meals/>
+        <Drinks/>
+
         <Footer2/>
     </div>
->>>>>>> 565d1ab3c9dae06364261d3e85503859d29c615c
   );
 }
 
