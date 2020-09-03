0e532fc540a561d269a42dcb0bed497eb75f9709
sofiene <chihisofiene2@gmail.com>
2020-09-03 01:59:01 +0100

    Updates

---
+++ precache-manifest.d96118de7dceecceef3de1a9e3ade695.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09badd32ae296e28c21d8eb6dda593ab",
+    "url": "/restoProject/index.html"
+  },
+  {
+    "revision": "1134eb8c043893f9588a",
+    "url": "/restoProject/static/css/2.8cd54ee5.chunk.css"
+  },
+  {
+    "revision": "3ceb7136d89992a15988",
+    "url": "/restoProject/static/css/main.b2734dd4.chunk.css"
+  },
+  {
+    "revision": "1134eb8c043893f9588a",
+    "url": "/restoProject/static/js/2.87390722.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/restoProject/static/js/2.87390722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ceb7136d89992a15988",
+    "url": "/restoProject/static/js/main.f73f8770.chunk.js"
+  },
+  {
+    "revision": "6f0df57125c026d7a6e0",
+    "url": "/restoProject/static/js/runtime-main.604b649b.js"
+  },
+  {
+    "revision": "38bf3d86be4d511fe71005a6d0a5dfe6",
+    "url": "/restoProject/static/media/abc.38bf3d86.jpg"
+  },
+  {
+    "revision": "d9a10890ee54ca44fbf8adcccabc3b2a",
+    "url": "/restoProject/static/media/background1.d9a10890.jpg"
+  },
+  {
+    "revision": "6884588dd6b1f168d89555f3d4d75763",
+    "url": "/restoProject/static/media/background2.6884588d.jpg"
+  },
+  {
+    "revision": "63213d166747fcef8e0623a57a7c9c52",
+    "url": "/restoProject/static/media/background3.63213d16.jpg"
+  },
+  {
+    "revision": "3c1ef3579bf7660861dda958362c2d55",
+    "url": "/restoProject/static/media/background4.3c1ef357.jpg"
+  },
+  {
+    "revision": "3c1ef3579bf7660861dda958362c2d55",
+    "url": "/restoProject/static/media/background5.3c1ef357.jpg"
+  },
+  {
+    "revision": "dd4567b4800f1de0dc75667aa6b28942",
+    "url": "/restoProject/static/media/background6.dd4567b4.jpg"
+  },
+  {
+    "revision": "284809d7f316a90380c801f0f150d656",
+    "url": "/restoProject/static/media/background7.284809d7.jpg"
+  },
+  {
+    "revision": "c974658bfd57abf35df791b293af3802",
+    "url": "/restoProject/static/media/background8.c974658b.jpg"
+  },
+  {
+    "revision": "dd4567b4800f1de0dc75667aa6b28942",
+    "url": "/restoProject/static/media/background9.dd4567b4.jpg"
+  },
+  {
+    "revision": "948b0e0bbe5c9831e21a5ee6552bd365",
+    "url": "/restoProject/static/media/bruger0.948b0e0b.jpg"
+  },
+  {
+    "revision": "05e8305a6e92e2a897efbacebc969a81",
+    "url": "/restoProject/static/media/build2.05e8305a.jpg"
+  },
+  {
+    "revision": "fdb953e747052b088eda7e2d8bc116b9",
+    "url": "/restoProject/static/media/burger.fdb953e7.png"
+  },
+  {
+    "revision": "c4a7c163e1dd453c7fedf1ff35ee0943",
+    "url": "/restoProject/static/media/burger1.c4a7c163.jpg"
+  },
+  {
+    "revision": "3ae6bd3030c4794d133e66a235db56bc",
+    "url": "/restoProject/static/media/burger10.3ae6bd30.jpg"
+  },
+  {
+    "revision": "f13d6bb06519e6a673300885e6ce5c2e",
+    "url": "/restoProject/static/media/burger2.f13d6bb0.jpg"
+  },
+  {
+    "revision": "1dfdecd220e66f834e407acb23f2ba7d",
+    "url": "/restoProject/static/media/burger3.1dfdecd2.jpg"
+  },
+  {
+    "revision": "bfc03eabc362843f8d7fd6f145811ca7",
+    "url": "/restoProject/static/media/burger4.bfc03eab.jpg"
+  },
+  {
+    "revision": "7daf0fdcd940367779f18ac7ce1f6d89",
+    "url": "/restoProject/static/media/burger5.7daf0fdc.jpg"
+  },
+  {
+    "revision": "eb0bf4f2100d41f28dedfc9fe10f74b6",
+    "url": "/restoProject/static/media/burger6.eb0bf4f2.jpg"
+  },
+  {
+    "revision": "a0187e427e361c5c28dbfb437aa14d00",
+    "url": "/restoProject/static/media/burger7.a0187e42.jpg"
+  },
+  {
+    "revision": "1222a4cfc2c60f716bd358d7f9d4d3fa",
+    "url": "/restoProject/static/media/burger8.1222a4cf.jpg"
+  },
+  {
+    "revision": "343332d61c47fceac664a74c6642beb2",
+    "url": "/restoProject/static/media/burger9.343332d6.jpg"
+  },
+  {
+    "revision": "432b8dd6fa6e0d38f4e88f5c27881080",
+    "url": "/restoProject/static/media/coca.432b8dd6.jpg"
+  },
+  {
+    "revision": "f7aaf55cc3d18b95503439d5105e1ba1",
+    "url": "/restoProject/static/media/contest1.f7aaf55c.jpg"
+  },
+  {
+    "revision": "1e1f2dd17f6b7458978e733160ea50e6",
+    "url": "/restoProject/static/media/dessert1.1e1f2dd1.jpg"
+  },
+  {
+    "revision": "782b22a91657d54198e71fdd27ee8fd3",
+    "url": "/restoProject/static/media/dessert2.782b22a9.jpg"
+  },
+  {
+    "revision": "eb4c349ec05fd8b178f870a50227b449",
+    "url": "/restoProject/static/media/dessert3.eb4c349e.jpg"
+  },
+  {
+    "revision": "81b74f15e011175017347d725754e02a",
+    "url": "/restoProject/static/media/dessert4.81b74f15.jpg"
+  },
+  {
+    "revision": "1cdc7896c6fec0e9894858f6383066a4",
+    "url": "/restoProject/static/media/dessert5.1cdc7896.jpg"
+  },
+  {
+    "revision": "061bd213000bd1078b74c4f8a68c33e9",
+    "url": "/restoProject/static/media/fast-food.061bd213.png"
+  },
+  {
+    "revision": "8290266a5fb402a34b96f890bbdb2d60",
+    "url": "/restoProject/static/media/insta.8290266a.png"
+  },
+  {
+    "revision": "f69d74760585c8d60c12376f0fb96cc3",
+    "url": "/restoProject/static/media/logo4.f69d7476.png"
+  },
+  {
+    "revision": "3da5ef4a7f082c1fd97569dbd8501455",
+    "url": "/restoProject/static/media/marker.3da5ef4a.png"
+  },
+  {
+    "revision": "514d1032f7ab5603edf5a220c8b72b41",
+    "url": "/restoProject/static/media/photoContest2.514d1032.jpg"
+  },
+  {
+    "revision": "97e0b626af0051475f7d06255b5f8a3c",
+    "url": "/restoProject/static/media/pizza.97e0b626.jpg"
+  },
+  {
+    "revision": "0b7fb60ad934ec0e705b54780dbee344",
+    "url": "/restoProject/static/media/pizza1.0b7fb60a.jpg"
+  },
+  {
+    "revision": "d1e8a0b00e1f0fd7afdc7f77def5f6c0",
+    "url": "/restoProject/static/media/pizza10.d1e8a0b0.jpg"
+  },
+  {
+    "revision": "2969fbf683b3fbda9ed042bcced6c440",
+    "url": "/restoProject/static/media/pizza11.2969fbf6.jpg"
+  },
+  {
+    "revision": "adecfb53d93d1979017adc64b2c5d98d",
+    "url": "/restoProject/static/media/pizza12.adecfb53.jpg"
+  },
+  {
+    "revision": "e1c756195387e5a86e2ce9a8846e4570",
+    "url": "/restoProject/static/media/pizza2.e1c75619.jpg"
+  },
+  {
+    "revision": "83bdc2997c2868d389a8163614c11207",
+    "url": "/restoProject/static/media/pizza3.83bdc299.jpg"
+  },
+  {
+    "revision": "32b67c728ad8b691637e6832b8b11cef",
+    "url": "/restoProject/static/media/pizza4.32b67c72.jpg"
+  },
+  {
+    "revision": "9ab76076317088735ff6b76007efca8b",
+    "url": "/restoProject/static/media/pizza5.9ab76076.jpg"
+  },
+  {
+    "revision": "1f031cb754036949ae864b1cb98dd007",
+    "url": "/restoProject/static/media/pizza6.1f031cb7.jpg"
+  },
+  {
+    "revision": "b6f2232d7e4d54202ff559004b604199",
+    "url": "/restoProject/static/media/pizza7.b6f2232d.jpg"
+  },
+  {
+    "revision": "e5e8489a5c26c7c71220a8d7112dfae0",
+    "url": "/restoProject/static/media/pizza8.e5e8489a.jpg"
+  },
+  {
+    "revision": "896ed73a7c1af2c0bc47f343a633808c",
+    "url": "/restoProject/static/media/pizza9.896ed73a.jpg"
+  },
+  {
+    "revision": "e539000eb44a2639dd0463d7e278b8ef",
+    "url": "/restoProject/static/media/restaurant.e539000e.jpg"
+  },
+  {
+    "revision": "8f35a40403a84631c4125c4f1859c7a6",
+    "url": "/restoProject/static/media/twitter.8f35a404.png"
+  }
+]);
