a9640e066113a0723950592cc9450e497957df31
sofiene <chihisofiene2@gmail.com>
2020-09-03 02:06:40 +0100

    with burger image

--- src/component/BuildBurger.js
+++ src/component/BuildBurger.js
@@ -6,12 +6,11 @@ import '../App.css';
 
 function BuildBurger() {
     return (
-<<<<<<< HEAD
     <div>
         <img id="buildPageImage" src={require("./images/Build/build2.jpg")}></img>
         <h1 id="buildPageTitle"  > build your own burgers </h1>
         <Card style={{ width: '18rem' }} className="container">
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Img variant="top" src={require('./images/Menu/burgers/burger1.jpg')} />
             <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
@@ -32,12 +31,6 @@ function BuildBurger() {
         <div style={{height:'400px'}}></div>
  
     </div>
-=======
-        <div>
-            <div style={{height:'68.5px'}}></div>
-            <h1>Build your burger motherfucker</h1>
-        </div>
->>>>>>> 32c4751e06cba9b82b36bb9f97683a1643c1e9d5
     )
 }
 
